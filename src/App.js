92ce7619a9851c8a38cd22c24371876b79884316
Alonso Parra <alonsogparra@gmail.com>
2020-09-22 17:46:32 -0400

    App updated with translate and react-router-dom, and component added Navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Navigation } from './components';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { I18nProvider, LOCALES } from './i18n';
+
+
 
 function App() {
+
+  const [locale, setLocale] = useState(LOCALES.ENGLISH);
+  const isEnglish = useSelector((state) => state.translate.isEnglish);
+
+  useEffect(() => {
+    if (isEnglish) {
+      setLocale(LOCALES.ENGLISH);
+    } else {
+      setLocale(LOCALES.SPANISH);
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <I18nProvider locale={locale}>
+     <BrowserRouter basename="/">
+      <Navigation />
+     </BrowserRouter>
+    </I18nProvider>
   );
 }
 
