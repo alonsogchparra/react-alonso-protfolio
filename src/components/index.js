a8495770ee13a964c754e9aae005027d2247219e
Alonso Parra <alonsogparra@gmail.com>
2020-09-24 09:19:02 -0400

    Resume added to components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navigation } from "./Navigation/Navigation";
 export { default as Dashboard } from "./Dashboard/Dashboard";
 export { default as About } from './About/About';
+export { default as Resume } from './Resume/Resume';
