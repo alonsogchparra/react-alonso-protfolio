27629f67782313ecbbd1a4d8e4c5fcf91726f97a
Alonso Parra <alonsogparra@gmail.com>
2021-03-25 21:34:09 -0400

    New component built Experience

---
+++ src/components/Resume/Experience.js
@@ -0,0 +1,36 @@
+import { Avatar, Box, Grid, Typography } from "@material-ui/core";
+import React from "react";
+import translate from "../../i18n/translate";
+
+const Experience = ({ imgLogo, place, logoSize, time, profession }) => {
+  return (
+    <>
+      <Box m={2}>
+        <Grid container justify="center" alignItems="center">
+          <Avatar src={imgLogo} className={logoSize} />
+        </Grid>
+        <Typography variant="h5" align="center" className="resume-college">
+          {place}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          className="resume-college-time"
+        >
+          {time}
+        </Typography>
+        {profession ? (
+          <Typography variant="body1" align="center" className="resume-college">
+            {translate("devFrontend")}
+          </Typography>
+        ) : (
+          <Typography variant="body1" align="center" className="resume-college">
+            {translate("devGame")}, {translate("illustrator")}
+          </Typography>
+        )}
+      </Box>
+    </>
+  );
+};
+
+export default Experience;
