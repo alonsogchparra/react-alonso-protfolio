e6fdda09e4c56847e3f74ba1b05aaf8f2d1d4f38
Alonso Parra <alonsogparra@gmail.com>
2022-02-15 18:42:10 -0400

    Experience updated with currently info

--- src/components/Resume/Experience.js
+++ src/components/Resume/Experience.js
@@ -1,31 +1,39 @@
-import { Avatar, Box, Grid, Typography } from "@material-ui/core";
-import React from "react";
-import translate from "../../i18n/translate";
+import { Avatar, Box, Grid, Typography } from '@material-ui/core';
+import React from 'react';
+import translate from '../../i18n/translate';
 
-const Experience = ({ imgLogo, place, logoSize, time, profession }) => {
+const Experience = ({
+  imgLogo,
+  place,
+  logoSize,
+  time,
+  profession,
+  currentJob = false,
+}) => {
   return (
     <>
       <Box m={2}>
-        <Grid container justify="center" alignItems="center">
+        <Grid container justify='center' alignItems='center'>
           <Avatar src={imgLogo} className={logoSize} />
         </Grid>
-        <Typography variant="h5" align="center" className="resume-college">
+        <Typography variant='h5' align='center' className='resume-college'>
           {place}
         </Typography>
         <Typography
-          variant="subtitle1"
-          align="center"
-          className="resume-college-time"
+          variant='subtitle1'
+          align='center'
+          className='resume-college-time'
         >
           {time}
+          {currentJob ? translate('currently') : null}
         </Typography>
         {profession ? (
-          <Typography variant="body1" align="center" className="resume-college">
-            {translate("devFrontend")}
+          <Typography variant='body1' align='center' className='resume-college'>
+            {translate('devFrontend')}
           </Typography>
         ) : (
-          <Typography variant="body1" align="center" className="resume-college">
-            {translate("devGame")}, {translate("illustrator")}
+          <Typography variant='body1' align='center' className='resume-college'>
+            {translate('devGame')}, {translate('illustrator')}
           </Typography>
         )}
       </Box>
