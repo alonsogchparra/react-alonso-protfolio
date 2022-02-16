6e3c78115c172d802deea26e7c388c7e0740f572
Alonso Parra <alonsogparra@gmail.com>
2022-02-15 21:05:45 -0400

    Links updated

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -19,6 +19,7 @@ import { useSelector } from 'react-redux';
 import translate from '../../i18n/translate';
 import Experience from './Experience';
 import Technologies from './Technologies';
+import { RESUMEN } from '../../constants/Links';
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
@@ -40,10 +41,6 @@ const useStyles = makeStyles((theme) => ({
 
 const Resume = () => {
   const classes = useStyles();
-  const cv_en =
-    'https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/curriculums%2Falonso_cv_2021_ENG.pdf?alt=media&token=17f6fc33-dc47-4202-8bce-c46d5a1c7097';
-  const cv_es =
-    'https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/curriculums%2Falonso_cv_2021_ESP.pdf?alt=media&token=51b95b0c-7039-4234-81aa-c0d86993803f';
   const isEnglish = useSelector((state) => state.translate.isEnglish);
 
   return (
@@ -61,7 +58,7 @@ const Resume = () => {
               <Box>
                 <Avatar
                   alt='Alonso Parra'
-                  src='https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/profile%2Fprofile_alonso_2-min.png?alt=media&token=00aa4a9e-2292-4d3d-a809-fb669cfdab66'
+                  src={RESUMEN.PROFILE_PHOTO}
                   className={classes.large}
                 />
               </Box>
@@ -80,7 +77,7 @@ const Resume = () => {
               </Box>
               <Box m={1}>
                 <a
-                  href={isEnglish ? cv_en : cv_es}
+                  href={isEnglish ? RESUMEN.CV_ENGLISH : RESUMEN.CV_SPANISH}
                   rel='noopener noreferrer'
                   target='_blank'
                 >
@@ -116,10 +113,7 @@ const Resume = () => {
                   alignItems='center'
                   style={{ marginBottom: '10px' }}
                 >
-                  <Avatar
-                    src='https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/studies%2Fula_logo.png?alt=media&token=4bf603ec-da62-4084-8bfd-f02d9a3d989b'
-                    className={classes.large}
-                  />
+                  <Avatar src={RESUMEN.ULA} className={classes.large} />
                 </Grid>
                 <Typography
                   variant='h5'
@@ -165,7 +159,7 @@ const Resume = () => {
               className='animate__animated animate__fadeIn'
             >
               <Experience
-                imgLogo='https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/experiences%2Fuakami.png?alt=media&token=73ece3e4-1b6c-4bfe-b886-ee5cb8026580'
+                imgLogo={RESUMEN.UAKAMI}
                 logoSize={classes.medium}
                 place='Uakami Studios'
                 time='2016 - Jun 2017'
@@ -173,7 +167,7 @@ const Resume = () => {
               />
 
               <Experience
-                imgLogo='https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/experiences%2Flikinvent.png?alt=media&token=fa1631f7-7efa-4828-a146-4adeef2363d7'
+                imgLogo={RESUMEN.LIKINVENT}
                 logoSize={classes.medium}
                 place='Likinvent Studios'
                 time='2015 - 2017'
@@ -181,7 +175,7 @@ const Resume = () => {
               />
 
               <Experience
-                imgLogo='https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/experiences%2Fvascar.png?alt=media&token=68d748d8-1e7c-4eb6-825d-6ca8e53b3926'
+                imgLogo={RESUMEN.VASCAR}
                 logoSize={classes.medium}
                 place='Vascar Solutions'
                 time='Jul 2017 - Mar 2018'
@@ -189,7 +183,7 @@ const Resume = () => {
               />
 
               <Experience
-                imgLogo='https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/experiences%2Fgoapps.png?alt=media&token=d58d8d5c-9d10-4f22-9105-4186c9349924'
+                imgLogo={RESUMEN.GO_APPS}
                 logoSize={classes.medium}
                 place='Go Apps'
                 time='Nov 2018 - Mar 2019'
@@ -197,7 +191,7 @@ const Resume = () => {
               />
 
               <Experience
-                imgLogo='https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/experiences%2Fqaroni.png?alt=media&token=00904a10-5524-4139-a4a7-74b0cfaa7949'
+                imgLogo={RESUMEN.QARONI}
                 logoSize={classes.medium}
                 place='Qaroní'
                 time='Oct 2020 - Mar 2021'
@@ -205,7 +199,7 @@ const Resume = () => {
               />
 
               <Experience
-                imgLogo='https://firebasestorage.googleapis.com/v0/b/fir-react-upload-files.appspot.com/o/experiences%2Farkus.png?alt=media&token=c76103e8-4427-4668-acc4-901e9bdbd414'
+                imgLogo={RESUMEN.ARKUSNEXUS}
                 logoSize={classes.medium}
                 place='Arkusnexus'
                 time='Jul 2021 - '
