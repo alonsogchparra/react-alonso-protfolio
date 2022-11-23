50783c715321d4bd261f9b89f979b45fcdf68747
Alonso Parra <alonsogparra@gmail.com>
2022-11-23 15:29:02 -0400

    FullScreen on videos added

--- src/components/Projects/ShowProjectPersonal.js
+++ src/components/Projects/ShowProjectPersonal.js
@@ -67,7 +67,7 @@ const ShowProjectPersonal = ({
                 <iframe
                   src={video}
                   frameBorder='0'
-                  allow='autoplay; encrypted-media'
+                  allow='autoplay; encrypted-media; fullscreen'
                   title='video'
                   width='100%'
                   height='250'
